a4ab91bb0478378608d46b69731c25d3406476a7
dextor <dextor@7643f386-bd4a-48d6-b6bf-a524f848cf1d>
2011-11-07 09:18:32 +0000

    completato script di get delle librerie
    git-svn-id: https://subversion.assembla.com/svn/megadevs/trunk/SocialWrapper@34 7643f386-bd4a-48d6-b6bf-a524f848cf1d

--- libs/libs.sh
+++ libs/libs.sh
@@ -4,6 +4,13 @@
 shopt -s extglob
 
 SCRIPT_NAME=`basename $0`
+TOBEKEPT=$SCRIPT_NAME
+
+
+##############################
+# 			TWITTER			 #
+##############################
+
 TWITTER4J_DOWNLOAD_WEBSITE="http://twitter4j.org/en/"
 TWITTER4J_LATEST_RELEASE="twitter4j-android-2.2.5.zip"
 TWITTER4J_DESIRED_LIB="core"
@@ -27,26 +34,63 @@ echo 'Checking for Twitter libs..'
 echo '--'
 
 if [ -f $TWITTER4J_CORE_LIB -o -f $TWITTER4J_LATEST_RELEASE ];
-	then echo '>> Twitter4J already present, skipping to next library';
+	then echo '>> Twitter4J libs are already present, skipping to next library';
 else
 	echo '>> Twitter4J not found, downloading..'
 	echo '--'
 	wget $TWITTER4J_DOWNLOAD_WEBSITE$TWITTER4J_LATEST_RELEASE
 	
-	echo '>> Twitter$J successfully downloaded. Unzipping..'
+	echo '>> Twitter4J successfully downloaded. Unzipping..'
 	echo '--'
 	unzip $TWITTER4J_LATEST_RELEASE >/dev/null
 	
 	echo '>> Unzipped. Moving library and cleaning up..'
 	echo '--'
 	mv lib/$TWITTER4J_CORE_LIB .
-	rm -rf !($SCRIPT_NAME|$TWITTER4J_CORE_LIB)	
+	TOBEKEPT=$TOBEKEPT"|"$TWITTER4J_CORE_LIB
+	echo $TOBEKEPT
+	rm -rf !($TOBEKEPT)	
 
 	echo -e 'Twitter4J successfully downloaded. Moving to next library.. \n'
 fi
 
-echo '---------------------'
+##############################
+# 			TUMBLR			 #
+##############################
 
+HTTPCLIENT_URL="http://apache.bfeel.it//httpcomponents/httpclient/binary/httpcomponents-client-4.1.2-bin.zip"
+HTTPCLIENT_ZIP="httpcomponents-client-4.1.2-bin.zip"
+HTTCLIENT_BASE_DIR="httpcomponents-client-4.1.2"
+HTTPCLIENT_LIB="httpclient-4.1.2.jar"
+SIGNPOST_URL="http://oauth-signpost.googlecode.com/files/"
+SIGNPOST_CORE="signpost-core-1.2.1.1.jar"
+SIGNPOST_COMMONS="signpost-commonshttp4-1.2.1.1.jar"
+
+echo '---------------------'
+echo 'Checking for Tumblr libs..'
+echo '--'
+
+if [ -f $HTTPCLIENT_LIB -a -f $SIGNPOST_CORE -a -f $SIGNPOST_COMMONS ] ;
+	then echo '>> Tumblr libs are already present, skipping to next library';
+else
+	echo '>> Tumblr libs not found, downloading..'
+	echo '--'
+	wget $HTTPCLIENT_URL
+	wget $SIGNPOST_URL$SIGNPOST_CORE
+	wget $SIGNPOST_URL$SIGNPOST_COMMONS
+	
+	echo '>> Packages successfully downloaded. Unzipping..'
+	echo '--'
+	unzip $HTTPCLIENT_ZIP > /dev/null
+	
+	echo '>> Unzipped. Moving library and cleaning up..'
+	echo '--'
+	mv $HTTCLIENT_BASE_DIR/lib/$HTTPCLIENT_LIB .
+	TOBEKEPT=$TOBEKEPT"|"$HTTPCLIENT_LIB"|"$SIGNPOST_CORE"|"$SIGNPOST_COMMONS
+	rm -rf !($TOBEKEPT)	
+
+	echo -e 'Tumblr successfully downloaded. Moving to next library.. \n'
+fi
 
 #find . -regex ".*\(sh\|jar\)$" | xargs rm -rf
 #find . ! -iname $SCRIPT_NAME -o -iname $TWITTER4J_CORE_LIB -exec -rm -rf;
